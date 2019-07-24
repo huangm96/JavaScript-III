9ac8e2bee1b684d8e69d37368cde24e0310088e8
unknown <minh.huang96@gmail.com>
2019-07-24 14:27:27 -0700

    finished prototypes

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -15,14 +15,36 @@
   * dimensions (These represent the character's size in the video game)
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
+function GameObject(g){
+  this.createdAt = g.createdAt;
+  this.name = g.name;
+  this.dimensions = 
+  {
+     length:g.l,
+    width:g.w,
+    height: g.h,
+    
+  }
+  };
 
+GameObject.prototype.destroy=function(){
+  return `${this.name} was removed from the game.`;
+};
 /*
   === CharacterStats ===
   * healthPoints
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
-
+function CharacterStats(c){
+  GameObject.call(this, c);
+  this.healthPoints = c.healthPoints;
+  
+}
+CharacterStats.prototype = Object.create(GameObject.prototype);
+CharacterStats.prototype.takeDamage=function(){
+  return `${this.name} took damage.`;
+};
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
   * team
@@ -32,7 +54,17 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
+ function Humanoid(h){
+  CharacterStats.call(this, h);
+  this.team = h.team;
+  this.weapons = h.weapons;
+  this.language = h.language;
+ }
+ Humanoid.prototype = Object.create(CharacterStats.prototype);
  
+ Humanoid.prototype.greet=function(){
+  return `${this.name} offers a greeting in ${this.language}.`
+ };
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -41,7 +73,7 @@
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -95,16 +127,89 @@
   console.log(mage.createdAt); // Today's date
   console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
   console.log(swordsman.healthPoints); // 15
-  console.log(mage.name); // Bruce
+ console.log(mage.name); // Bruce
   console.log(swordsman.team); // The Round Table
   console.log(mage.weapons); // Staff of Shamalama
   console.log(archer.language); // Elvish
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
   // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villain and one a hero and fight it out with methods!
+
+  const Troll = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 1,
+      width: 2,
+      height: 1,
+    },
+    healthPoints: 20,
+    name: 'Troll',
+    team: 'Horde',
+    weapons: [
+      'broadsword',
+    ],
+    language: 'Elvish',
+  });
+
+  const Worgen = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 1,
+      width: 2,
+      height: 3,
+    },
+    healthPoints: 18,
+    name: 'Worgen',
+    team: 'Alliance',
+    weapons: [
+      'broadsword',
+    ],
+    language: 'Elvish',
+  });
+
+  GameObject.prototype.attack=function(enemy, power){
+    let point=0;
+    if(power=="final hit"){
+      point=5;
+    }else{
+      point =2;
+    }
+    enemy.healthPoints -=point;
+    if(enemy.healthPoints>0){
+      return`${this.name} attacks ${enemy.name},${enemy.name}'health point is ${enemy.healthPoints}. `;
+    }else{
+      return ` ${enemy.name} dies`;
+    }
+  };
+  GameObject.prototype.recover=function(){
+    
+    this.healthPoints +=3;
+    
+      return`${this.name}'s health point +3, ${this.name}'health point is ${this.healthPoints}. `;
+    
+  };
+  console.log("\nstretch problem"); 
+  console.log(Worgen.attack(Troll,"regular attack")); 
+  console.log(Troll.attack(Worgen,"regular attack"));
+  console.log(Worgen.attack(Troll,"final hit"));
+  console.log(Troll.recover());
+  console.log(Worgen.attack(Troll,"regular attack")); 
+  console.log(Troll.attack(Worgen,"final hit"));
+  console.log(Worgen.recover());
+  console.log(Troll.attack(Worgen,"regular attack"));
+  console.log(Worgen.attack(Troll,"final hit"));
+  console.log(Troll.attack(Worgen,"final hit"));
+  console.log(Worgen.recover());
+  console.log(Troll.attack(Worgen,"regular attack"));
+  console.log(Worgen.attack(Troll,"final hit"));
+  console.log(Troll.attack(Worgen,"final hit"));
+  console.log(Worgen.attack(Troll,"final hit"));
+  
+
+  
